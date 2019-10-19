97a5bf0bcae937c99c91be47917d39980c360451
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-19 23:21:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -25,7 +25,7 @@ def apply_coupons(cart, coupons)
     if cart.has_key?(item) 
       if cart[item][:count] >= coupon[:num]
         if !cart [coupon_item]
-           cart[coupon_item] = {count: coupon[:num], price: coupon[:cost]/coupon[:num]
+           cart[coupon_item] = {count: coupon[:num], price: coupon[:cost]/coupon[:num]}
         else
         cart[coupon_item][:count] += coupon[:num]
         end
