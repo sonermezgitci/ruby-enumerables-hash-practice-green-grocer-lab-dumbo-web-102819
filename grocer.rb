88693776953bb10f5d199e28ea52a4e5e1856d3f
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-19 22:19:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,8 +37,8 @@ end
 
 def apply_clearance(cart)
   cart.each do |product_name,stats|
-    stats[:price] -= stats[:price] *0.2 if stats[:clearance] == true
-  binding.pry
+    stats[:price] -= stats[:price] *0.2 if stats[:clearance]
+ 
   end 
     cart
 end
