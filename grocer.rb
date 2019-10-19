d56026606c6f09aa5d3683bd0abab6f18321a91b
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-19 22:41:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,5 +46,5 @@ def checkout(cart, coupons)
   applied_coupons = applied_coupons(hash_cart,coupons)
   applied_discount = apply_clearance(applied_coupons)
   total = applied_discount.reduce(0) { |acc,(key,value)|acc += value[:price] * value[:count]}
-  
+  total> 100 ? total * 0.9 : total
 end
