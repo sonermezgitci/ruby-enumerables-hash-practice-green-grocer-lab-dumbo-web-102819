61a3975706979b2272f7fda22644590add54f702
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-19 23:34:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,9 @@
   require 'pry'
   
-  def consolidate_cart(cart)
-    final_hash = {}
-    cart.each do |element_hash|
-      element_name = element_hash.keys[0]
+    def consolidate_cart(cart)
+      final_hash = {}
+      cart.each do |element_hash|
+        element_name = element_hash.keys[0]
       
       if final_hash.has_key?(element_name)
        final_hash[element_name][:count] += 1
