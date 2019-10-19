2196d8e38d609fa8688632abef172474383101d0
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-19 19:16:45 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,26 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  final_hash = {}
+  cart.each do |element_hash|
+    element_name = element_hash.keys[0]
+    
+    if final_hash.has_key?(element_name)
+      final_hash[element_name[:count] += 1 
+    else 
+      final_hash[element_name] = {
+          count: 1,
+          price: element_hash[element_name][:price],
+          clearance: element_hash[element_name][:clearance]
+      }
+    end
+   end
+ final_hash
 end
+
+
+
+
 
 def apply_coupons(cart, coupons)
   # code here
