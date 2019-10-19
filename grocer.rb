2594ce1303d092b35f06259dfc17b7f18bbaccfe
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-19 23:35:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,22 +1,22 @@
   require 'pry'
   
-    def consolidate_cart(cart)
-      final_hash = {}
-      cart.each do |element_hash|
-        element_name = element_hash.keys[0]
+def consolidate_cart(cart)
+  final_hash = {}
+  cart.each do |element_hash|
+  element_name = element_hash.keys[0]
       
-      if final_hash.has_key?(element_name)
-       final_hash[element_name][:count] += 1
-      else 
-        final_hash[element_name] = {
-            count: 1,
-            price: element_hash[element_name][:price],
-            clearance: element_hash[element_name][:clearance]
-        }
-      end
-     end
-   final_hash
-  end
+    if final_hash.has_key?(element_name)
+    final_hash[element_name][:count] += 1
+        else 
+          final_hash[element_name] = {
+              count: 1,
+              price: element_hash[element_name][:price],
+              clearance: element_hash[element_name][:clearance]
+          }
+        end
+       end
+     final_hash
+    end
   
   def apply_coupons(cart, coupons)
     coupons.each do |coupon|
